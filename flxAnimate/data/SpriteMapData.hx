3a929599d65e7693f23bf5c61ffb8eff0cce7809
MrCheemsAndFriends <84867412+Cheemsandfriends@users.noreply.github.com>
2022-01-30 14:54:26 +0100

    Add again the stuff

---
+++ flxAnimate/data/SpriteMapData.hx
@@ -0,0 +1,37 @@
+package flxAnimate.data;
+
+typedef AnimateAtlas = {
+    var ATLAS:AnimateSprites;
+	var meta:MetaStuff;
+};
+
+typedef AnimateSprites = {
+    var SPRITES:Array<AnimateSprite>;
+};
+
+typedef AnimateSprite = {
+    var SPRITE:AnimateSpriteData;
+};
+
+typedef MetaStuff = {
+	var app:String;
+	var version:String;
+	var image:String;
+	var format:String;
+	var size:SizeMeta;
+	var resolution:String;
+}
+
+typedef SizeMeta = 
+{
+	var w:Float;
+	var h:Float;
+}
+typedef AnimateSpriteData = {
+    var name:String;
+    var x:Float;
+    var y:Float;
+    var w:Float;
+    var h:Float;
+	var rotated:Bool;
+};
