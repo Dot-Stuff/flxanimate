bfaa26e229a1803298f159724efd0ab471ca63b0
MrCheemsAndFriends <84867412+Cheemsandfriends@users.noreply.github.com>
2022-02-13 17:01:32 +0100

    The button support(?)

--- flxanimate/FlxSpriteMap.hx
+++ flxanimate/FlxSpriteMap.hx
@@ -3,6 +3,17 @@ package flxanimate;
 import flixel.FlxSprite;
 import flixel.graphics.frames.FlxFramesCollection;
 
+typedef Settings = {
+	?ButtonSettings:ButtonSettings,
+	?FrameRate:Float,
+	?Reversed:Bool,
+	?OnComplete:Void->Void,
+}
+typedef ButtonSettings = {
+	?OnClick:Void->Void,
+	?Sound:FlxSound
+}
+
 class FlxSpriteMap extends FlxSprite
 {
 	/**
@@ -14,7 +25,15 @@ class FlxSpriteMap extends FlxSprite
 	 * Internal, the first frame of the `FlxSpriteMap`.
 	 */
 	var anim(default, null):FlxAnim;
-
+	
+	public var onClick:Void->Void;
+	
+	public var onComplete:Void->Void;
+	
+	public var reversed:Bool  = false;
+	
+	var badPress:Bool = false;
+	
 	/**
 	 * Internal, used for each skip between frames.
 	 */
@@ -35,15 +54,15 @@ class FlxSpriteMap extends FlxSprite
 	 * @param Path 		
 	 * @param Framerate The initial framerate of the texture sheet.
 	 */
-	public function new(?X:Float = 0, ?Y:Float = 0, Path:String, ?Framerate:Int = 0)
+	public function new(?X:Float = 0, ?Y:Float = 0, Path:String, ?Framerate:Int = 0, , ?Settings:Settings)
 	{
 		var jsontxt = haxe.Json.parse(openfl.Assets.getText('$Path/Animation.json'));
 		anim = new FlxAnim(X, Y, jsontxt);
 		anim.setLayers();
-
-		framerate = Framerate >= 0 ? anim.coolParse.MD.FRT : Framerate;
-
+		setTheSettings(Settings);
 		super();
+		
+		anim.frames = FlxAnimateFrames.fromAnimate(Path);
 	}
 
 	public override function draw()
@@ -65,6 +84,22 @@ class FlxSpriteMap extends FlxSprite
 
 	public function playAnim(reverse:Bool = false)
 	{
+		reversed = reverse;
+		@:privateAccess
+		if (reversed)
+		{
+			if (anim.curFrame >= 0)
+			{
+				anim.curFrame = anim.length;
+			}
+		}
+		else
+		{
+			if (anim.curFrame >= anim.length)
+			{
+				anim.curFrame = 0;
+			} 
+		}
 		isPlaying = true;
 	}
 
@@ -81,20 +116,70 @@ class FlxSpriteMap extends FlxSprite
 
 	public override function update(elapsed:Float)
 	{
-		if (!isPlaying)
-			return;
-
-		frameTick += elapsed;
-
-		if (frameTick >= 1 / framerate)
+		if (anim.symbolType == BUTTON)
 		{
-			anim.curFrame += 1;
-			frameTick = 0;
+			if (FlxG.mouse.pressed && !FlxG.mouse.overlaps(anim) && !badPress)
+			{
+				badPress = true;
+			}
+			if (FlxG.mouse.released && badPress)
+			{
+				badPress = false;
+			}
+			@:privateAccess
+			anim.setButtonFrames(anim, badPress);
 		}
+		else
+		{
+			@:privateAccess
+			if (!isPlaying)
+				return;
+			else
+			{
+				frameTick += elapsed;
 
+				while (frameTick > framerate)
+				{
+					if (reversed)
+					{
+						anim.curFrame--;
+					}
+					else
+					{
+						anim.curFrame++;
+					}
+					frameTick = 0;
+				}
+				if (reversed && anim.curFrame == 0 && anim.loopType == LOOP)
+				{
+					anim.curFrame = anim.length;
+				}
+			}
+			@:privateAccess
+			if (onComplete != null && isPlaying && anim.loopType == PLAY_ONCE)
+			{
+				if (reversed)
+				{
+					if (anim.curFrame <= 0)
+					{
+						onComplete();
+						isPlaying = false;
+					}
+				}
+				else
+				{
+					if (anim.curFrame >= anim.length)
+					{
+						onComplete();
+						isPlaying = false;	
+					}
+					
+				}
+			}
+		}
 		super.update(elapsed);
 	}
-
+	// why??
 	@:noCompletion
 	override function set_frames(Frames:FlxFramesCollection):FlxFramesCollection
 	{
@@ -110,5 +195,36 @@ class FlxSpriteMap extends FlxSprite
 		}
 
 		return Frames;
+	}
+	function setTheSettings(?Settings:Settings)
+	{
+		framerate = 1 / anim.coolParse.MD.FRT;
+		if (Settings != null)
+		{
+			if (Settings.ButtonSettings != null)
+			{
+				if (Settings.ButtonSettings.OnClick != null)
+				{
+					onClick = Settings.ButtonSettings.OnClick;
+				}
+				if (Settings.ButtonSettings.Sound != null)
+				{
+					sound = Settings.ButtonSettings.Sound;
+				}
+				anim.symbolType = BUTTON;
+			}
+			if (Settings.Reversed != null)
+			{
+				reversed = Settings.Reversed;
+			}
+			if (Settings.FrameRate != null)
+			{
+				framerate = 1 / (Settings.FrameRate > 0 ? anim.coolParse.MD.FRT : Settings.FrameRate);
+			}
+			if (Settings.OnComplete != null)
+			{
+				onComplete = Settings.OnComplete;
+			}
+		}
 	}
 }
