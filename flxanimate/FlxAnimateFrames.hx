2336b3bcb765377f175001f3c103de0d86c7a742
MrCheemsAndFriends <84867412+Cheemsandfriends@users.noreply.github.com>
2022-02-13 14:19:39 +0100

    Adding the support for more than one spritemap and also for resolution shit

--- flxanimate/FlxAnimateFrames.hx
+++ flxanimate/FlxAnimateFrames.hx
@@ -1,53 +1,101 @@
 package flxanimate;
 
+import flixel.math.FlxMatrix;
 import flixel.FlxG;
 import flixel.graphics.FlxGraphic;
 import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.graphics.frames.FlxFrame.FlxFrameAngle;
 import flixel.math.FlxPoint;
 import flixel.math.FlxRect;
-import flixel.system.FlxAssets.FlxGraphicAsset;
 import flxanimate.data.SpriteMapData.AnimateAtlas;
 import flxanimate.data.SpriteMapData.AnimateSpriteData;
 import openfl.Assets;
-import openfl.geom.Rectangle;
+import openfl.display.BitmapData;
+import flxanimate.data.AnimationData;
 
-class FlxAnimateFrames
+class FlxAnimateFrames extends FlxAtlasFrames
 {
+    static var data:AnimateAtlas = null;
     /**
      * Parsing method for Animate Texture Atlases
      * 
-     * @param Source        The image source (can be `FlxGraphic`, `String` or `BitmapData`).
-     * @param Description   Contents of the JSON file with atlas description.
-	 *                      You can get it with `Assets.getText(path/to/description.json)`.
-	 *                      Or you can just pass a path to the JSON file in the assets directory.
+     * @param Path          Set the Path where the Sprites are.
      * @return              Newly created `FlxAtlasFrames` collection.
      */
-    public static function fromAnimate(Source:FlxGraphicAsset, Description:String):FlxAtlasFrames
+    public static function fromAnimate(Path:String):FlxAtlasFrames
     {
-        var graphic:FlxGraphic = FlxG.bitmap.add(Source);
-        if (graphic == null)
-            return null;
-
-        var bitmapResult:FlxAtlasFrames = new FlxAtlasFrames(graphic);
-
-        if (Assets.exists(Description))
-            Description = Assets.getText(Description);
-
-        var data:AnimateAtlas = haxe.Json.parse(StringTools.replace(Description, String.fromCharCode(0xFEFF), ""));
-
-        for (i in data.ATLAS.SPRITES)
+        var bitmap:BitmapData = null;
+        if (Assets.exists('$Path/spritemap1.json'))
         {
-            var data:AnimateSpriteData = i.SPRITE;
-            var frame:FlxRect = FlxRect.get(data.x, data.y, data.w, data.h);
-            var rectangleSize:Rectangle = new Rectangle(0, 0, frame.width, frame.height);
+            data = haxe.Json.parse(StringTools.replace(Assets.getText('$Path/spritemap1.json'), String.fromCharCode(0xFEFF), ""));
+        }
+        else if (Assets.exists('$Path/spritemap.json'))
+        {
+            data = haxe.Json.parse(StringTools.replace(Assets.getText('$Path/spritemap.json'), String.fromCharCode(0xFEFF), ""));
+        }
+        
+        if (data == null)
+            return null;
+        if (Assets.exists('$Path/spritemap1.png'))
+        {
+            var i:Int = 1;
+            bitmap = Assets.getBitmapData('$Path/spritemap1.png');
+            while (Assets.exists('$Path/spritemap$i.png'))
+            {
+                if (i > 1)
+                {
+                    var data2:AnimateAtlas = haxe.Json.parse(StringTools.replace(Assets.getText('$Path/spritemap$i.json'), String.fromCharCode(0xFEFF), ""));
 
-            var offset:FlxPoint = FlxPoint.get(-rectangleSize.left, -rectangleSize.top);
-            var angle = data.rotated ? FlxFrameAngle.ANGLE_NEG_90 : FlxFrameAngle.ANGLE_0;
-            var sourceSize:FlxPoint = FlxPoint.get(rectangleSize.width, rectangleSize.height);
-            bitmapResult.addAtlasFrame(frame, sourceSize, offset, data.name, angle);
+                    for (e in data2.ATLAS.SPRITES)
+                    {
+                        e.SPRITE.y += bitmap.height;
+                        data.ATLAS.SPRITES.push(e);
+                    }
+
+                    var bitmap2 = Assets.getBitmapData('$Path/spritemap$i.png');
+                    var bitmapDraw = new BitmapData(bitmap.width + bitmap2.width, max(bitmap.height, bitmap2.height), true, 0x00000000);
+                    bitmapDraw.draw(bitmap);
+                    bitmapDraw.draw(bitmap2, new FlxMatrix(1,0,0,1, bitmap.width, 0));
+                    
+                    bitmap = bitmapDraw;
+                }
+                i++;
+            }
+        }
+        else if (Assets.exists('$Path/spritemap.png'))
+        {
+            bitmap = Assets.getBitmapData('$Path/spritemap.png');
+        }
+        
+        if (bitmap == null)
+            return null;
+        AnimationData.version = data.meta.version;
+        var graphic:FlxGraphic = FlxG.bitmap.add(bitmap);
+
+        var frames:FlxAtlasFrames = new FlxAtlasFrames(graphic);
+        
+        for (sprites in data.ATLAS.SPRITES)
+        {
+            textureAtlasHelper(sprites.SPRITE.name, sprites.SPRITE, frames);
         }
 
-        return bitmapResult;
+        return frames;
     }
+
+
+    static function textureAtlasHelper(FrameName:String, FrameData:AnimateSpriteData, Frames:FlxAtlasFrames):Void
+    {
+        var rotated:Bool = FrameData.rotated;
+        var name:String = FrameName;
+        var angle:FlxFrameAngle = (rotated) ? FlxFrameAngle.ANGLE_NEG_90 : FlxFrameAngle.ANGLE_0;
+        var frameRect:FlxRect = (rotated) ? FlxRect.get(FrameData.x, FrameData.y, FrameData.h, FrameData.w) : FlxRect.get(FrameData.x, FrameData.y, FrameData.w, FrameData.h);
+        var sourceSize:FlxPoint = FlxPoint.get(frameRect.width / Std.parseInt(data.meta.resolution), frameRect.height / Std.parseInt(data.meta.resolution));
+        
+        var offset:FlxPoint = FlxPoint.get(0, 0);
+
+        Frames.addAtlasFrame(frameRect, sourceSize, offset, name, angle);
+    }
+	//adding a max for Int stuff
+    static function max(a:Int, b:Int):Int
+		return a < b ? b : a;
 }
