ec7ff4a99a4a56c724fc418fdf9ce7d59c90fcb5
MrCheemsAndFriends <84867412+Cheemsandfriends@users.noreply.github.com>
2022-03-13 22:54:31 +0100

    I discovered about fmt.hdll. Lime doesn't give you that so it's lime's problem lol

--- flxanimate/zip/Zip.hx
+++ flxanimate/zip/Zip.hx
@@ -45,15 +45,13 @@ class Zip
         var r = new Reader(i);
         return r.read();
     }
-    // I'm trying to make like a custom zip thing so that it's easy to make in every framework, cos it's "Haxe-made", but hl is another story cos well...
-    // It needs an hdll, tbs, the fmt.hdll, but idk where to download that, neither include in a project, so I'm gonna try to unzip shit without that
     public static function unzip(f:List<Entry>):List<Entry>
     {
         for (list in f)
         {
             if (list.compressed)
             {
-                #if !hl
+                #if !lime
                 var s = haxe.io.Bytes.alloc(list.fileSize);
                 var c = new haxe.zip.Uncompress(-15);
                 var r = c.execute(list.data, 0, s, 0);
@@ -62,8 +60,7 @@ class Zip
                     throw "Invalid compressed data for " + list.fileName;
                 list.data = s;
                 #else
-                trace("NO HASHLINK FULL SUPPORT!!!!!!");
-                list.data = Deflate.decompress(list.data);
+                    list.data = Deflate.decompress(list.data);
                 #end
                 list.compressed = false;
                 list.dataSize = list.fileSize;
