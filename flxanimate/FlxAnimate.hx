42f2c3eb3358a9c658ab110c72c4f56b5150b7c1
MrCheemsAndFriends <84867412+Cheemsandfriends@users.noreply.github.com>
2022-03-22 19:59:22 +0100

    Fix other shit idk

--- flxanimate/FlxAnimate.hx
+++ flxanimate/FlxAnimate.hx
@@ -1,5 +1,6 @@
 package flxanimate;
 
+import flxanimate.animate.FlxAnim.ButtonEvent;
 import flxanimate.zip.Zip;
 import openfl.Assets;
 import haxe.io.BytesInput;
@@ -16,7 +17,8 @@ typedef Settings = {
 	?FrameRate:Float,
 	?Reversed:Bool,
 	?OnComplete:Void->Void,
-	?ShowPivot:Bool
+	?ShowPivot:Bool,
+	?Antialiasing:Bool
 }
 typedef ButtonSettings = {
 	?OnClick:Void->Void,
@@ -72,13 +74,8 @@ class FlxAnimate extends FlxSprite
 	public function new(X:Float = 0, Y:Float = 0, Path:String, ?Settings:Settings)
 	{
 		super(X, Y);
-
-		/**
-		 * A massive meteor was found in New Mexico.
-		 * At first, it was thought that this was some type of hoax, but now the U.S. Geological Survey has confirmed that it is indeed a natural meteorite.
-		 */
-		makeGraphic(16, 16, 0);
-
+		makeGraphic(16,16,0);
+		
 		if (!Assets.exists('$Path/Animation.json') && haxe.io.Path.extension(Path) != "zip")
 		{
 			FlxG.log.error('Animation file hasnt been found in Path $Path, Have you written the correct Path?');
@@ -101,11 +98,10 @@ class FlxAnimate extends FlxSprite
 			anim.cameras = cameras;
 			anim.x = x;
 			anim.y = y;
+			anim.offset = offset;
 			anim.scrollFactor = scrollFactor;
 			anim.xFlip = flipX;
 			anim.yFlip = flipY;
-			anim.OnClick = onClick;
-			anim.Sound = sound;
 			anim.renderFrames(timeline);
 		}
 		super.draw();
@@ -119,14 +115,21 @@ class FlxAnimate extends FlxSprite
 	public function playAnim(?Name:String, ForceRestart:Bool = false, Looped:Bool = false, Reverse:Bool = false, flipX:Bool = false, flipY:Bool = false)
 	{
 		@:privateAccess
-		if ([null, ""].indexOf(Name) == -1)
+		var curThing = anim.animsMap.get(Name);
+		@:privateAccess
+		if (curThing != null && anim.name != Name || ForceRestart || !Reverse && anim.curFrame >= anim.length || Reverse && anim.curFrame <= 0)
 		{
-			var curThing = anim.animsMap.get(Name);
-			if (curThing != null && anim.name != Name)
+			if (!Reverse)
 				anim.curFrame = 0;
+			else
+				anim.curFrame = anim.length;
+		}
+		@:privateAccess
+		if ([null, ""].indexOf(Name) == -1 && curThing != null)
+		{
 			anim.x = x;
 			anim.y = y;
-			timeline = (curThing != null || curThing.timeline != null) ? curThing.timeline : anim.coolParse.AN.TL;
+			timeline = curThing.timeline;
 			if (curThing != null)
 			{
 				anim.x += curThing.X;
@@ -152,13 +155,12 @@ class FlxAnimate extends FlxSprite
 	{
 		isPlaying = false;
 	}
-
 	public function stopAnim()
 	{
 		pauseAnim();
 		anim.curFrame = 0;
 	}
-
+	
 	function set_framerate(value:Float):Float
 	{
 		frameDelay = 1 / value;
@@ -173,15 +175,6 @@ class FlxAnimate extends FlxSprite
 		{
 			new ButtonEvent(onClick, sound).fire();
 			anim.clickedButton = false;
-		}
-		@:privateAccess
-		if (anim.curLabel != null)
-		{
-			if (anim.labelcallbacks.exists(anim.curLabel))
-			{
-				for (callback in anim.labelcallbacks.get(anim.curLabel))
-					callback();
-			}
 		}
 		@:privateAccess
 		if (!isPlaying)
@@ -204,13 +197,23 @@ class FlxAnimate extends FlxSprite
 			}
 		}
 		@:privateAccess
-		if (onComplete != null && [playonce, "playonce"].indexOf(anim.loopType) != -1)
+		if (anim.curLabel != null)
+		{
+			if (anim.labelcallbacks.exists(anim.curLabel))
+			{
+				for (callback in anim.labelcallbacks.get(anim.curLabel))
+					callback();
+			}
+		}
+		@:privateAccess
+		if ([playonce, "playonce"].indexOf(anim.loopType) != -1)
 		{
 			if (reversed)
 			{
 				if (anim.curFrame <= 0)
 				{
-					onComplete();
+					if (onComplete != null)
+						onComplete();
 					isPlaying = false;
 				}
 			}
@@ -218,7 +221,8 @@ class FlxAnimate extends FlxSprite
 			{
 				if (anim.curFrame >= anim.length)
 				{
-					onComplete();
+					if (onComplete != null)
+						onComplete();
 					isPlaying = false;	
 				}
 			}
@@ -240,24 +244,19 @@ class FlxAnimate extends FlxSprite
 				{
 					sound = Settings.ButtonSettings.Sound;
 				}
-				anim.symbolType = button;
+				if ([button, "button"].indexOf(anim.symbolType) == -1)
+					anim.symbolType = button;
 			}
 			if (Settings.Reversed != null)
-			{
 				reversed = Settings.Reversed;
-			}
 			if (Settings.FrameRate != null)
-			{
 				framerate = (Settings.FrameRate > 0 ? anim.coolParse.MD.FRT : Settings.FrameRate);
-			}
 			if (Settings.OnComplete != null)
-			{
 				onComplete = Settings.OnComplete;
-			}
 			if (Settings.ShowPivot != null)
-			{
 				showPivot = Settings.ShowPivot;
-			}
+			if (Settings.Antialiasing != null)
+				antialiasing = Settings.Antialiasing;
 		}
 	}
 	function atlasSetting(Path:String):AnimAtlas
@@ -266,13 +265,18 @@ class FlxAnimate extends FlxSprite
 		if (haxe.io.Path.extension(Path) == "zip")
 		{
 			var thing = Zip.readZip(new BytesInput(Assets.getBytes(Path)));
+			
 			for (list in Zip.unzip(thing))
 			{
 				if (list.fileName.indexOf("Animation.json") != -1)
 				{
 					jsontxt = haxe.Json.parse(list.data.toString());
+					thing.remove(list);
+					break;
 				}
 			}
+			@:privateAccess
+			FlxSpriteMap.zip = thing;
 		}
 		else
 		{
