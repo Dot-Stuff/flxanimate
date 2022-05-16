7b5b71cab88bcf9a9485845072b28a6fba17e60b
CheemsAndFriends <granchinvictor@gmail.com>
2022-05-16 22:07:08 +0200

    Changing the filter thing

--- flxanimate/Filters.hx
+++ flxanimate/Filters.hx
@@ -4,71 +4,92 @@ import flixel.system.FlxAssets.FlxShader;
 
 class Filters
 {
-	public var shader(default, null):FilterShader;
-    public var hue(default, null):Float = 0;
-	public var saturation(default, null):Float = 0;
+	public var shader(default, null):#if !flash FilterShader #else Dynamic #end;
+
+    public var hue(get, set):Float;
+	public var saturation(get, set):Float;
+	public var brightness(get, set):Float;
 
 	public function new():Void
 	{
+		
 		shader = new FilterShader();
+		shader.HSV.value = [0, 1, 1];
         hue = 0;
 		saturation = 0;
-        shader.hue.value = [hue];
-		shader.saturation.value = [saturation];
+		brightness = 0;
 	}
 
-	public function setHue(hue:Float):Void
+	function get_hue()
 	{
-		this.hue = hue;
-		shader.hue.value[0] = hue;
+		return shader.HSV.value[0] * 360;
 	}
-	public function setSaturation(saturation:Float):Void
+	function set_hue(hue:Float)
 	{
-		this.saturation = saturation;
-		shader.saturation.value[0] = saturation;
+		shader.HSV.value[0] = hue / 360;
+		return hue;
+	}
+	function get_saturation()
+	{
+		return (shader.HSV.value[1] * 100) - 100;
+	}
+	function set_saturation(saturation:Float)
+	{
+		shader.HSV.value[1] = 1 + (saturation / 100);
+		return saturation;
+	}
+	function get_brightness()
+	{
+		return (shader.HSV.value[2] * 100) - 100;
+	}
+	function set_brightness(brightness:Float)
+	{
+		shader.HSV.value[2] = 1 + (brightness / 100);
+		return brightness;
 	}
 }
 
 class FilterShader extends FlxShader
 {
 	@:glFragmentSource('
-		#pragma header
-		uniform float hue;
-		uniform float saturation;
+	#pragma header
+	uniform vec3 HSV;
+	
+	vec3 rgb2hsv(vec3 c)
+	{
+		vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+		vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+		vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
 
-		vec3 rgb2hsv(vec3 c)
-		{
-			vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
-			vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
-			vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+		float d = q.x - min(q.w, q.y);
+		float e = 1.0e-10;
+		return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+	}
 
-			float d = q.x - min(q.w, q.y);
-			float e = 1.0e-10;
-			return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
-		}
-
-		vec3 hsv2rgb(vec3 c)
-		{
-			vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-			vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-			return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
-		}
-
-		void main()
-		{
-			vec4 color = flixel_texture2D(bitmap, openfl_TextureCoordv);
-
-			vec3 hsv = rgb2hsv(vec3(color[0], color[1], color[2]));
-			// [0] == hue
-			hsv[0] = hue;
-			// [1] == saturation?
-			hsv[1] += saturation;
-
-			color = vec4(hsv2rgb(hsv), color[3]);
+	vec3 hsv2rgb(vec3 c)
+	{
+		vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+		vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+		return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
 		
-			
-			gl_FragColor = color;
-		}')
+		// if accumulating the output, cyan, magenta, yellow, especially pastels
+		// and of course whites are overexposed much sooner
+		// than mostly-saturated red, green, or blue
+		// a work around is by normalizing the last mix before applying the gain, like so:
+		// return c.z * normalize(mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y));
+	}
+
+	void main(){
+		vec4 textureColor = flixel_texture2D(bitmap, openfl_TextureCoordv);
+		vec3 fragRGB = textureColor.rgb;
+		vec3 fragHSV = rgb2hsv(fragRGB).xyz;
+		fragHSV.x += mod(HSV.x, 1.0);
+		fragHSV.yz *= HSV.yz;
+
+		fragRGB = hsv2rgb(fragHSV);
+		gl_FragColor = vec4(fragRGB, textureColor.w);
+	}
+	')
 	public function new()
 	{
 		super();
